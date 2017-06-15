d07183058d440ccbfe1e7a26ed8aeaa54bdc144f
onoie <onoie3@gmail.com>
2017-06-16 04:01:58 +0900

    add boost temp

---
+++ boost/README.md
@@ -0,0 +1,10 @@
+# boost
+```
+2017/06/16
+author:onoie
+ubuntu16
+CLion2017.01
+Qt5.9
+```
+`cmake-build-debug is not found -> project reload`
+`sudo apt-get install libboost-all-dev`
---
+++ boost/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <boost/version.hpp>
+using namespace std;
+int main() {
+    cout << (BOOST_VERSION / 100000) << "." << (BOOST_VERSION / 100 % 1000) << "." << (BOOST_VERSION % 100) << endl;
+    return 0;
+}
